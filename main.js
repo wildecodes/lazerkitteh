abf7b4dfb25cbd8e2e5028232344ee569dbd5b89
🦄 <_>
2020-01-31 15:42:21 -0500

    fixed gamepiece color

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
+var myGamePiece;
+
 function startGame() {
   myGameArea.start();
-  myGamePiece = new component(30, 30, "red", 200, 570);
+  myGamePiece = new component(30, 30, "white", 285, 570);
   document.onkeydown = handle;
-  // myGamePiece.gravity = 0.05;
-  // myScore = new component("30px", "Consolas", "black", 280, 40, "text");
-  // myGameArea.start();
+  updateGameArea();
 }
-
-
-var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-  bgReady = true;
-};
-bgImage.src = "background.jpg";
 
 var myGameArea = {
   canvas: document.createElement("canvas"),
+  // canvas.setAttribute("id", "canvas"),
   start: function () {
-    this.canvas.width = 480;
+    this.canvas.width = 600;
     this.canvas.height = 600;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
@@ -26,6 +19,18 @@ var myGameArea = {
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function () {
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
@@ -47,7 +52,6 @@ function movePiece(direction) {
   updateGameArea();
 }
 
-
 function handle(e) {
   switch (e.keyCode) {
     case 37:
@@ -64,18 +68,6 @@ function handle(e) {
       break;
   }
 };
-
-function component(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.x = x;
-  this.y = y;
-  this.update = function () {
-    ctx = myGameArea.context;
-    ctx.fillstyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  };
-}
 
 function updateGameArea() {
   myGameArea.clear();
