f2916a015a95f01316256a512f3caff5047c2cd3
🦄 <_>
2020-02-05 14:42:31 -0500

    improved styling, file structure, rendering

--- main.js
+++ main.js
@@ -1,75 +1,82 @@
-var myGamePiece;
+var hero;
 
 function startGame() {
-  myGameArea.start();
-  myGamePiece = new component(30, 30, "white", 285, 570);
-  document.onkeydown = handle;
-  updateGameArea();
+    gameArea.start();
+    image = new Image();
+    image.src = './assets/hero.jpg';
+    hero = new component(image, 30, 30, 285, 570);
+    document.onkeydown = handle;
+    hero.update();
+    updateGameArea();
 }
 
-var myGameArea = {
-  canvas: document.createElement("canvas"),
-  // canvas.setAttribute("id", "canvas"),
-  start: function () {
-    this.canvas.width = 600;
-    this.canvas.height = 600;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    // this.interval = setInterval(updateGameArea, 20);
-  },
-  clear: function () {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-}
+var gameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.setAttribute("id", "canvas");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        // this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
 
-function component(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.x = x;
-  this.y = y;
-  this.update = function () {
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
+function component(img, width, height, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.img = img;
+    this.update = function () {
+        ctx = gameArea.context;
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    };
 }
 
 function movePiece(direction) {
-  switch (direction) {
-    case 'left':
-      myGamePiece.x -= 10;
-      break;
-    case 'right':
-      myGamePiece.x += 10;
-      break;
-    case 'up':
-      myGamePiece.y -= 10;
-      break;
-    case 'down':
-      myGamePiece.y += 10;
-      break;
-  }
-  updateGameArea();
+    switch (direction) {
+        case 'left': hero.x -= 10;
+            break;
+        case 'right': hero.x += 10;
+            break;
+        case 'up': hero.y -= 10;
+            break;
+        case 'down': hero.y += 10;
+            break;
+    }
+    updateGameArea();
+}
+
+function lazer() { // lazer = new component(30, 5, "red", hero.x, hero.y);
+    console.log("pewpew!");
 }
 
 function handle(e) {
-  switch (e.keyCode) {
-    case 37:
-      movePiece('left');
-      break;
-    case 38:
-      movePiece('up');
-      break;
-    case 39:
-      movePiece('right');
-      break;
-    case 40:
-      movePiece('down');
-      break;
-  }
-};
+    switch (e.keyCode) {
+        case 37: movePiece('left');
+            e.preventDefault();
+            break;
+        case 38: movePiece('up');
+            e.preventDefault();
+            break;
+        case 39: movePiece('right');
+            e.preventDefault();
+            break;
+        case 40: movePiece('down');
+            e.preventDefault();
+            break;
+        case 70: lazer();
+            e.preventDefault();
+            // console.log("pew...");
+            break;
+    }
+}
 
 function updateGameArea() {
-  myGameArea.clear();
-  myGamePiece.update();
+    gameArea.clear();
+    hero.update();
 }
